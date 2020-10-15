925933130522a454d44f2f9eb6b588b3b2f61e65
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2020-10-15 11:01:08 -0500

    I did some work

---
+++ Delaware Dash/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody playerRb;
+    public float speed = 50;
+    public float horizontalInput;
+    public float verticalInput;
+    public Vector3 playerPos;
+    
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(transform.forward * speed * horizontalInput * Time.deltaTime);
+        playerRb.AddForce(transform.right * -speed * verticalInput * Time.deltaTime);
+        
+    }
+}
