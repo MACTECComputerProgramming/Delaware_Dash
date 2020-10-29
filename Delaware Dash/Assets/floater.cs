ff8946544e27b0854d3c2111148f98c2d6e2aa13
MessyAttorney16 <70332474+MessyAttorney16@users.noreply.github.com>
2020-10-29 11:07:28 -0500

    water work

---
+++ Delaware Dash/Assets/floater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floater : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+
+    private void FixedUpdate()
+    {
+        float waveHeight = waveManager.instance.GetWaveHeight(transform.position.x);
+        if (transform.position.y < 0f)
+        {
+            float displacementMultiplier = Mathf.Clamp01(-transform.position.y / depthBeforeSubmerged) * displacementAmount;
+            rigidBody.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
+        }
+    }
+}
