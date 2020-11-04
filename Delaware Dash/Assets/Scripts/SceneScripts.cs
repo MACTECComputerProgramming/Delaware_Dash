cde65f97a78b013b4b0dd3ccaa34e259496dcf88
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2020-11-04 00:58:48 -0600

    This is amazing!
    I figured out how to do menu stuff and switching between scenes!

---
+++ Delaware Dash/Assets/Scripts/SceneScripts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScripts : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+
+}
