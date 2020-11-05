e8b01814f9e0db044ee304cb7a71c4137ff36259
MessyAttorney16 <70332474+MessyAttorney16@users.noreply.github.com>
2020-11-05 11:32:20 -0600

    boat and water work

---
+++ Delaware Dash/Assets/floater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floater : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+    public int floaterCount = 1;
+    public float waterDrag = 0.99f;
+    public float waterAngularDrag = 0.5f;
+    public GameObject waterplane;
+
+    private void FixedUpdate()
+    {
+        rigidBody.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+        float waveHeight = (waveManager.instance.GetWaveHeight(transform.position.x)) + waterplane.transform.position.y;
+        if (transform.position.y < waterplane.transform.position.y)
+        {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            rigidBody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            rigidBody.AddForce(displacementMultiplier * -rigidBody.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rigidBody.AddTorque(displacementMultiplier * -rigidBody.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+
+    }   
+  
+}
